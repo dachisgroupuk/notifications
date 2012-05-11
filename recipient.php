20a359a3b38eca76a68dfc2d135cb17f52648871
Maarten Jacobs <maarten.jacobs@dachisgroup.com>
2012-05-11 16:15:47 +0100

    Updating package docs to notifications_api only

--- recipient.php
+++ recipient.php
@@ -1,4 +1,11 @@
 <?php
 
+/**
+ * Represents a single Recipient in the system.
+ * Highly recommended to inherit from, to represent Recipients in your system. 
+ *
+ * @package notifications_api
+ * @author Maarten Jacobs
+ **/
 class Recipient extends Actor {  
 }
--- sender.php
+++ sender.php
@@ -1,4 +1,11 @@
 <?php
 
+/**
+ * Represents a single Sender in the system.
+ * Highly recommended to inherit from, to represent Senders in your system. 
+ *
+ * @package notifications_api
+ * @author Maarten Jacobs
+ **/
 class Sender extends Actor {
 }
